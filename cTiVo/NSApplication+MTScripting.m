a460814fab6c75ea80b8293c89d12e479bc8665e
Thomas Lockney <thomas@lockney.net>
2026-05-02 22:23:32 -0700

    Add pause/resume processing to AppleScript
    Expose "processing paused" as a settable boolean property on the
    application, plus "pause processing" and "resume processing" commands
    as convenience aliases. Both back onto MTTiVoManager.processingPaused.

--- cTiVo/NSApplication+MTScripting.m
+++ cTiVo/NSApplication+MTScripting.m
@@ -16,6 +16,9 @@
 @property (nonatomic, readonly) NSArray<MTDownload *> *downloadQueue;
 @property (nonatomic, readonly) NSArray<MTFormat *> *formatList;
 @property (nonatomic, readonly) MTFormat *selectedFormat;
+@property (nonatomic) BOOL processingPaused;
+- (id)handlePauseProcessingCommand:(NSScriptCommand *)command;
+- (id)handleResumeProcessingCommand:(NSScriptCommand *)command;
 @end
 
 @implementation NSApplication (MTScripting)
@@ -42,6 +45,24 @@
 
 - (MTFormat *)selectedFormat {
     return tiVoManager.selectedFormat;
+}
+
+- (BOOL)processingPaused {
+    return tiVoManager.processingPaused.boolValue;
+}
+
+- (void)setProcessingPaused:(BOOL)paused {
+    tiVoManager.processingPaused = @(paused);
+}
+
+- (id)handlePauseProcessingCommand:(NSScriptCommand *)command {
+    self.processingPaused = YES;
+    return nil;
+}
+
+- (id)handleResumeProcessingCommand:(NSScriptCommand *)command {
+    self.processingPaused = NO;
+    return nil;
 }
 
 @end
